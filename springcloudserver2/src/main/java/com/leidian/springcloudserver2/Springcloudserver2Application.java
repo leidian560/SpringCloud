1bbb0f2884ae437937923f335656b6dc784bfa45
leidian560 <199005077@qq.com>
2018-09-12 20:01:13 +0800

    cloudservers

---
+++ springcloudserver2/src/main/java/com/leidian/springcloudserver2/Springcloudserver2Application.java
@@ -0,0 +1,12 @@
+package com.leidian.springcloudserver2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springcloudserver2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Springcloudserver2Application.class, args);
+    }
+}
